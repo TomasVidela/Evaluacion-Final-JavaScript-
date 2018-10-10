cdc7fb98ba4e36a1186605f9cc37800378f1cb34
TomasVidela <creativovidela@gmail.com>
2018-10-10 11:18:51 -0500

    cambiar signo

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var calculadora = {
   pantalla: document.getElementById('display'),
-  valorPantalla: "0",
+  valorPantalla: "",
   operacion: "",
   primerValor: 0,
   segundoValor: 0,
@@ -10,10 +10,10 @@ var calculadora = {
 
   init: (function(){
     this.asignarEfectoBotones(); // ejecutar EfectoBotones
-  //  this.asignarEventosBotones();// ejecutar EventosBotone
+    this.asignarEventosBotones();// ejecutar EventosBotone
   }),
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
   //Efectos de los botones
   asignarEfectoBotones: function(){
     var teclas = document.querySelectorAll(".tecla");
@@ -31,9 +31,110 @@ var calculadora = {
   MinimizaBoton: function(minimiza){
       minimiza.target.style="transform: scale(1,1)";
   },
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+asignarEventosBotones: function(){
+  document.getElementById("0").addEventListener("click", function(){ // escuchar el evento click  del documento e ingresar numero
+    calculadora.ingresarNumero("0");
+  });
+  document.getElementById("1").addEventListener("click", function(){
+    calculadora.ingresarNumero("1");
+  });
+  document.getElementById("2").addEventListener("click", function(){
+    calculadora.ingresarNumero("2");
+  });
+  document.getElementById("3").addEventListener("click", function(){
+    calculadora.ingresarNumero("3");
+  });
+  document.getElementById("4").addEventListener("click", function(){
+    calculadora.ingresarNumero("4");
+  });
+  document.getElementById("5").addEventListener("click", function(){
+    calculadora.ingresarNumero("5");
+  });
+  document.getElementById("6").addEventListener("click", function(){
+    calculadora.ingresarNumero("6");
+  });
+  document.getElementById("7").addEventListener("click", function(){
+    calculadora.ingresarNumero("7");
+  });
+  document.getElementById("8").addEventListener("click", function(){
+    calculadora.ingresarNumero("8");
+  });
+  document.getElementById("9").addEventListener("click", function(){
+    calculadora.ingresarNumero("9");
+  });
+  document.getElementById('on').addEventListener("click", function(){
+    calculadora.limpiarPantalla();
+  });
+  document.getElementById('sign').addEventListener("click", function(){
+    calculadora.cambiarSigno();
+  });
+  document.getElementById('punto').addEventListener("click", function(){
+    calculadora.puntoDecimal();
+  });
+  document.getElementById('raiz').addEventListener("click", function(){
+    calculadora.operaciones("raiz");
+  });
+  document.getElementById('dividido').addEventListener("click", function(){
+    calculadora.operaciones("/");
+  });
+  document.getElementById('por').addEventListener("click", function(){
+    calculadora.operaciones("*");
+  });
+  document.getElementById('menos').addEventListener("click", function(){
+    calculadora.operaciones("-");
+  });
+  document.getElementById('mas').addEventListener("click", function(){
+    calculadora.operaciones("+");
+  });
+  document.getElementById('igual').addEventListener("click", function(){
+    calculadora.resultadoPantalla();
+  });
 
+},
+///////////////////////////////////////////////////////////////////////////////
+actualizarPantalla: function(){
+  this.pantalla.innerHTML = this.valorPantalla; // ingresar Numero
+},
+///////////////////////////////////////////////////////////////////////////////
+ingresarNumero: function(numero){
 
+  if (this.valorPantalla.length < 9) {  //valor de la pantalla menor que 9 digitos
+    if(this.valorPantalla == "0"){  //si el valor de  la pantalla es igual a 0
+      this.valorPantalla = "";  // espacio para valor nuevo Valor
+      this.valorPantalla = this.valorPantalla + numero; // concatenar  valorde la pantalla  mas nuevo Valor
+    }else{
+      this.valorPantalla = this.valorPantalla + numero; // de otro modo concatenar valor de la pantalla  mas nuevo Valor
+    }
+    this.actualizarPantalla();
+  }
+},
+///////////////////////////////////////////////////////////////////////////////
+limpiarPantalla: function(){ //funcion que reinicia todos los valosres
+  this.valorPantalla = "0";
+  this.operacion = "0";
+  this.primerValor = 0;
+  this.segundoValor = 0;
+  this.resultado = 0;
+  this.teclaIgual = false;
+  this.ultimoValor = 0;
+  this.actualizarPantalla();
+},
+///////////////////////////////////////////////////////////////////////////////
+cambiarSigno: function(){
+  if (this.valorPantalla != "0") { // si el valor de la pantalla no es igual a 0
+    var signo;
+    if (this.valorPantalla.charAt(0) == "-") {
+      signo=this.valorPantalla.slice(1);
+    } else {
+      signo = "-" + this.valorPantalla;
+    }
+    this.valorPantalla = "";
+    this.valorPantalla = signo;
+    this.actualizarPantalla();
+  }
+},
+////////////////////////////////////////////////////////////////////////////////
 
 }
 calculadora.init();
